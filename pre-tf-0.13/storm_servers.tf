e03cdd4e0a97b8f94bd335d6abdca68906d7ca3a
Jack Hayhurst <jhayhurst@liquidweb.com>
2023-08-28 12:54:47 -0400

    committing what might should work?

---
+++ pre-tf-0.13/storm_servers.tf
@@ -0,0 +1,33 @@
+variable "liquidweb_config_path" {
+  type = "string"
+}
+
+provider "liquidweb" {
+  config_path = var.liquidweb_config_path
+}
+
+data "liquidweb_network_zone" "testing_zone" {
+  name        = "Zone B"
+  region_name = "US Central"
+}
+
+resource "liquidweb_storm_server" "testing_servers" {
+  count = 2
+
+  #config_id = "${data.liquidweb_storm_server_config.api.id}"
+  config_id = 1090
+  zone      = 12
+  #data.liquidweb_network_zone.api.id
+  template       = "UBUNTU_1804_UNMANAGED" // ubuntu 18.04
+  domain         = "servers.${count.index}.terraform-testing.api.jakdept.dev"
+  password       = "11111aA"
+  public_ssh_key = file("${path.root}/devkey.pub")
+}
+
+output "region_id" {
+  value = data.liquidweb_network_zone.testing_zone.region_id
+}
+
+output "instances" {
+  value = liquidweb_storm_server.testing_servers.*.ip
+}
