cfc3188d19f77bc190b4b7f7e49a79b0785635b2
Jack Hayhurst <jhayhurst@liquidweb.com>
2023-09-07 16:01:33 -0400

    moved stuff around, and added a cleanup script
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

---
+++ basic-example/provider.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  required_providers {
+    liquidweb = {
+      source = "registry.terraform.io/liquidweb/liquidweb"
+      version = "~> 1.6.2"
+    }
+  }
+}
--- current-tf/provider.tf
+++
@@ -1,9 +0,0 @@
-
-terraform {
-  required_providers {
-    liquidweb = {
-      source = "local.providers/liquidweb/liquidweb"
-      version = "~> 1.5.8"
-    }
-  }
-}
